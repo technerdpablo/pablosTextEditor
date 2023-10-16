e65271cee1787251d7ae726947894fa240bbefd2
pabloantonio <emailpabloroman@gmail.com>
2023-10-16 11:32:04 -0400

    updated files

---
+++ Develop/client/webpack.config.js
@@ -0,0 +1,73 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      // plug to generate html files
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: "J.A.T.E"
+      }),
+      // service worker 
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: "src-sw.js"
+      }),
+      // manifest file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Just another text editor',
+        background_color: '#34567a',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        crossorigin: 'anonymous', // crossorigin can be null, anonymous or use-credentials
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons')
+          }
+        ]
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // use babel-loader
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            }
+          }
+        }
+      ],
+    },
+  };
+};
---
+++ README.md
@@ -0,0 +1,11 @@
+# PWA Text Editor 
+
+# Description
+this is a simple text editor pwa application that allows user to add and fetch data from the database.
+
+# How to compile the application
+ - open the folder and go to the root directory  of the application `cd Develop`
+ - Install the dependencies `npm  install`
+ - run the  application `npm run start`
+ - check the browser on port `3000`.
+ 
